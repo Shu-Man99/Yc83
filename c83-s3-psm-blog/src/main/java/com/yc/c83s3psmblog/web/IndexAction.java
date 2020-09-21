d6f56f8517b10ad97f4f39146a8b95ae90ac0cdf
舍予 <@DESKTOP-57M3D7L>
2020-09-21 15:26:27 +0800

    文章发布页面

--- c83-s3-psm-blog/src/main/java/com/yc/c83s3psmblog/web/IndexAction.java
+++ c83-s3-psm-blog/src/main/java/com/yc/c83s3psmblog/web/IndexAction.java
@@ -37,4 +37,8 @@ public class IndexAction {
 		m.addAttribute("article", amapper.selectById(id));
 		return "article";
 	}
+	@GetMapping("addArticle")
+	public String addArticle() {
+		return "addArticle";
+	}
 }
